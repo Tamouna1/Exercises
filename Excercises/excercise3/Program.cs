2c63b6df53e11b029a5f975979cc8892ed5c6d43
Tamuna Janezashvili <95763995+Tamouna1@users.noreply.github.com>
2022-10-23 08:41:35 +0400

    days of week

---
+++ Excercises/excercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //3 days of week
+
+            DateTime dt = new DateTime(2022, 10, 23);
+            Console.WriteLine("Is Thursday the day of the week for {0:d}?: {1}",
+                               dt, dt.DayOfWeek == DayOfWeek.Thursday);
+            Console.WriteLine("The day of the week for {0:d} is {1}.", dt, dt.DayOfWeek);
+        }
+    }
+}
+    
+
+
