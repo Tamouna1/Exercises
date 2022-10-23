ff1ac10ca12fc46d28a73cca0b9d7c5fdf505dab
Tamuna Janezashvili <95763995+Tamouna1@users.noreply.github.com>
2022-10-23 07:55:03 +0400

    random numbers

---
+++ Excercises/excercise 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //2 Random Numbers
+
+            Random randomNumbers = new Random();
+
+            for (int i = 1; i <=10; i++)
+            {
+                int rand = randomNumbers.Next(100) + 100;
+                Console.WriteLine(rand);
+            }
+        }
+    }
+}
