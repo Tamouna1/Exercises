afe0bf8ba64bdc11e8901deae36b62ec7ee83c87
Tamuna Janezashvili <95763995+Tamouna1@users.noreply.github.com>
2022-10-26 14:23:19 +0400

    4

---
+++ Excercises/excercise 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            
+                int sum = 0;
+                int startTime = Environment.TickCount;
+                
+                for (int i = 0; i < 10000000; i++)
+                {
+                    sum++;
+                Console.WriteLine(sum);
+                }
+
+            }
+        }
+}
